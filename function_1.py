f57948dd93870a4265a03f763125f0f235e11f5b
litchiMIAU <69179208+litchiMIAU@users.noreply.github.com>
2020-10-29 11:56:47 +0800

    更新價格顯示方式

--- function_1.py
+++ function_1.py
@@ -22,24 +22,26 @@ def findShopee(page, keyword1, keyword2, keyword3):
     links = [i.find('a').get('href') for i in all_items]
     
     '''因為有些是價格範圍，所以要特別取出來'''
-    price = []
+    price1 = []
+    price2 = []
     for i in range(len(prices)):
         a = []
         for item in prices[i]:
             a.extend(list(item))
         if len(a)>2:
-            price.append('{}{} ~ {}{}'.format(a[0],a[1],a[5],a[6]))
+            price1.append(a[1])
+            price2.append(a[6])
         else:
-            price.append('{}{}'.format(a[0],a[1]))
+            price1.append(a[1])
+            price2.append(a[1])
             
-    
     '''寫入csv檔案'''
     with open('output{}.csv'.format(page), 'w', newline='', encoding='utf-8-sig') as csvfile:
-        fieldnames = ['商品', '價格', '網址']
+        fieldnames = ['商品', '價格1', '價格2', '網址']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
-        for i in range(len(price)):
-          writer.writerow({'商品': list(contents[i])[0], '價格': price[i], '網址': 'https://shopee.tw/'+links[i]})
+        for i in range(len(price1)):
+            writer.writerow({'商品': list(contents[i])[0], '價格1': price1[i], '價格2': price2[i], '網址': 'https://shopee.tw/'+links[i]})
 
         
 for i in range(5):
