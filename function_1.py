9d69eb4fd7a1941703b0cb0bda540ceb403fa359
windlist <collins110215@gmail.com>
2020-11-02 23:18:27 +0800

    新增gui.py
    1.改用pandas讀取csv檔(目前只讀取單個 迴圈還沒改)
    2.treeview已可顯示圖片
    3.按鍵+功能與框架美化另日修改
    4.function_1.py 中的 圖片下載已改為註解(目前用不到)

--- function_1.py
+++ function_1.py
@@ -1,8 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import os
+import time
 
-
+'''
+def download_img(link,i,page):
+    #圖片下載 #放置路徑可能需修改
+    download_path = './image{}/'.format(page)
+    if not os.path.isdir(download_path):
+        os.mkdir(download_path)
+    img = requests.get(link)# url 轉為圖片
+    with open(download_path+'shappee{}.jpg'.format(i),'wb') as f:
+        f.write(img.content)#content 寫進 file.jpg 放入資料夾
+'''
 # 爬取蝦皮網站
 def findShopee(page, keyword1, keyword2, keyword3):
     '''設定網址'''
@@ -16,11 +27,12 @@ def findShopee(page, keyword1, keyword2, keyword3):
     soup = BeautifulSoup(r.text, 'html.parser')
     
     '''設定抓取內容'''
+    pictures = soup.find_all("div",class_="_39-Tsj _1tDEiO")
+    pictures_link = [i.find('img').get('src') for i in pictures]
     contents = soup.find_all("div", class_="_1NoI8_ _16BAGk")
     prices = soup.find_all("div", class_="_1w9jLI _37ge-4 _2ZYSiu")
     all_items = soup.find_all("div", class_="col-xs-2-4 shopee-search-item-result__item")
     links = [i.find('a').get('href') for i in all_items]
-    
     '''因為有些是價格範圍，所以要特別取出來'''
     price1 = []
     price2 = []
@@ -34,20 +46,18 @@ def findShopee(page, keyword1, keyword2, keyword3):
         else:
             price1.append(a[1])
             price2.append(a[1])
-            
+
     '''寫入csv檔案'''
     with open('output{}.csv'.format(page), 'w', newline='', encoding='utf-8-sig') as csvfile:
-        fieldnames = ['商品', '價格1', '價格2', '網址']
+        fieldnames = ['圖片','商品', '價格1', '價格2', '網址']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
-<<<<<<< Updated upstream
         for i in range(len(price1)):
-            writer.writerow({'商品': list(contents[i])[0], '價格1': price1[i], '價格2': price2[i], '網址': 'https://shopee.tw/'+links[i]})
-
-        
-=======
-        for i in range(len(price)):
-          writer.writerow({'商品': list(contents[i])[0], '價格': price[i], '網址': 'https://shopee.tw/'+links[i]})
->>>>>>> Stashed changes
+            #download_img(picture_link[i]) #新增擺放圖片的資料夾
+            writer.writerow({'圖片':pictures_link[i],'商品': list(contents[i])[0], 
+                             '價格1': price1[i], '價格2': price2[i], 
+                             '網址': 'https://shopee.tw/'+links[i]})
+            time.sleep(0.2)
+    
 for i in range(5):
     findShopee(i, '日檢', 'N2', '單字')
---
+++ gui_pandas.py
@@ -0,0 +1,54 @@
+import io
+from tkinter import *
+from tkinter.ttk import *
+from PIL import Image, ImageTk
+from urllib.request import urlopen
+import time
+import os
+import pandas as pd
+# 參考網址 https://stackoverrun.com/cn/q/12976424
+
+root = Tk()
+root.title("Shopee_Gui")
+root.geometry("1260x780")
+Style().configure("Treeview",rowheight = 320)
+
+def readImage(url):
+    img_bytes = urlopen(url).read()#讀取url
+    data_stream = io.BytesIO(img_bytes)
+    pil_image = Image.open(data_stream)
+    return pil_image
+#建立捲動條
+scrollbar = Scrollbar(root, orient="vertical") 
+scrollbar.pack(side="right", fill="y") 
+#建立treeview(此height 代表顯示欄位數量 非欄位寬(rowheight))
+tree = Treeview(root,height = 3,columns = ("商品","價格","網址"),
+                yscrollcommand=scrollbar.set)
+#建立欄位開頭
+tree.heading("#0", text = "圖片")
+tree.heading("#1", text = "商品")
+tree.heading("#2", text = "價格")
+tree.heading("#3", text = "網址")
+#設定欄位寬度與顯示位置
+tree.column("#0",anchor = W,width = 320)
+tree.column("#1",anchor = W,width = 400)
+tree.column("#2",anchor = CENTER,width = 40)
+tree.column("#3",anchor = W,width = 500)
+
+putimage= []
+data = pd.read_csv('./output0.csv')#pandas 開啟 csv檔案
+for i in range(0,data.shape[0]):
+     pil_image = readImage(data.iloc[i][0])#跳去副函式 iloc[i][0]為圖片網址
+     pil_image2 = pil_image.resize((186,186),Image.ANTIALIAS)#resize 修改大小
+     imgobj = ImageTk.PhotoImage(pil_image2) #photoImage轉換
+     putimage.append(imgobj) #放進list
+     tree.insert("",'end',image = putimage[-1],values=(data.iloc[i][1],
+                                        data.iloc[i][2],data.iloc[i][4]))
+     #insert treeview
+     #注意 如果執行發現error:img???(???為任意數字) does not exist 
+     #請重新刪除 image = putimage[-1] 執行一次，並關閉程式後再補回執行
+     #每次圖片抓取[-1] : 因image是每次讀取一整個list, 
+     #每次list元素會增加，因此固定抓最後一個被放進的圖片
+tree.pack(side=TOP,fill=BOTH)
+scrollbar.config(command=tree.yview)  
+root.mainloop()
